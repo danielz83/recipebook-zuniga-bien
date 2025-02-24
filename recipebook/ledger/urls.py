a5206e96fa847ce5149343a6ceb5ae6a3d567c1c
Bien Zuniga <bien.zuniga@student.ateneo.edu>
2025-02-24 16:31:50 +0800

    added view for showing individual recipe details

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import recipes_list
+from .views import recipes_list, recipe_with_param
 
 urlpatterns = [
         path('recipes/list/', recipes_list, name='recipes_list'),
+        path('recipe/<int:num>/', recipe_with_param, name='recipe'),
         ]
 
 appname = "ledger"
