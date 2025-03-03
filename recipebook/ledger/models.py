f71b4b5339b486e4b493b536a708de98d81f69a3
Bien Zuniga <bien.zuniga@student.ateneo.edu>
2025-03-03 22:29:35 +0800

    added string representation and url to models

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -5,8 +5,17 @@ from django.db import models
 class Ingredient(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return str(self.name)
+
 class Recipe(models.Model):
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return str(self.name)
+
+    def get_absolute_url(self):
+        return reverse('recipe',args=[str(self.name)])
 
 class RecipeIngredient(models.Model):
     quantity = models.IntegerField()
