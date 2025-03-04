aa03f0bb78a98c210758f82590242d8a90fd87cf
Bien Zuniga <bien.zuniga@student.ateneo.edu>
2025-03-04 16:24:56 +0800

    Added model metadata

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -5,6 +5,10 @@ from django.db import models
 class Ingredient(models.Model):
     name = models.CharField(max_length=50)
 
+    class Meta:
+        ordering = ['name']
+        unique_together = ['name']
+
     def __str__(self):
         return str(self.name)
 
@@ -13,6 +17,10 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=50)
+
+    class Meta:
+        ordering = ['name']
+        unique_together = ['name']
 
     def __str__(self):
         return str(self.name)
@@ -24,3 +32,7 @@ class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=50)
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name="recipe")
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="ingredients")
+
+    class Meta:
+        ordering = ['ingredient']
+        unique_together = [['ingredient','recipe']]
