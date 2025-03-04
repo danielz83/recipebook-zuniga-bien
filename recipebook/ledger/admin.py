a38de96a5f73e7239aa1a319b81610e584eacac3
Bien Zuniga <bien.zuniga@student.ateneo.edu>
2025-03-04 15:33:11 +0800

    updated admiin panel

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Recipe, Ingredient, RecipeIngredient
 
 # Register your models here.
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+    list_display = ("name",)
+    list_filter = ("name",)
+
+class IngredientAdmin(admin.ModelAdmin):
+    model = Ingredient
+
+    list_display = ("name",)
+    list_filter = ("name",)
+
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    model = RecipeIngredient
+
+    list_display = ("quantity","ingredient","recipe",)
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
