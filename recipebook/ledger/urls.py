08e407a3462d8aa7e49bf6aa199106a49244524e
Bien Zuniga <bien.zuniga@student.ateneo.edu>
2025-03-27 17:32:35 +0800

    Added RecipeAddView and RecipeAddImageView

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import RecipeListView, RecipeDetailView
+from .views import RecipeListView, RecipeDetailView, RecipeAddView, RecipeAddImageView
 urlpatterns = [
         path('recipes/list/', RecipeListView.as_view(), name='recipes_list'),
         path('recipe/<int:pk>/', RecipeDetailView.as_view(), name='recipe_detail'),
+        path('recipe/add/', RecipeAddView.as_view(), name='recipe_add'),
+        path('recipe/<int:pk>/add_image', RecipeAddImageView.as_view(), name='recipe_add_image'),
         ]
 
 appname = "ledger"
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,19 +1,40 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.template import loader
-from .models import Recipe, RecipeIngredient, Ingredient
+from django.urls import reverse
+from .models import Recipe, RecipeIngredient, Ingredient, RecipeImage
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
 class RecipeListView(LoginRequiredMixin, ListView):
-    def get_queryset(self):
-        return Recipe.objects.filter(author__user=self.request.user)
+    model = Recipe
     template_name = 'ledger/recipes_list.html'
 
 class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = 'ledger/recipe_detail.html'
 
+class RecipeAddView(LoginRequiredMixin, CreateView):
+    model = Recipe
+    fields = ["name","author"]
+    template_name = 'ledger/recipe_add.html'
+
+class RecipeAddImageView(LoginRequiredMixin, CreateView): 
+    model = RecipeImage
+    fields = ["image","description"]
+    template_name = 'ledger/recipe_add_image.html'
+
+    def form_valid(self, form):
+        form.instance.recipe_id = self.kwargs["pk"]
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx["recipe"] = Recipe.objects.get(pk=self.kwargs["pk"])
+        return ctx
+
+    def get_success_url(self):
+        return reverse("recipe_detail", kwargs={"pk": self.kwargs["pk"]})
