35174163328e411ab3a8aa9cec4f75bfda17d960
Bien Zuniga <bien.zuniga@student.ateneo.edu>
2025-03-31 15:33:40 +0800

    Added add ingredient page to URLconf

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import RecipeListView, RecipeDetailView, RecipeAddView, RecipeAddImageView
+from .views import RecipeListView, RecipeDetailView, RecipeAddView, RecipeAddImageView, RecipeAddIngredientView
 urlpatterns = [
         path('recipes/list/', RecipeListView.as_view(), name='recipes_list'),
         path('recipe/<int:pk>/', RecipeDetailView.as_view(), name='recipe_detail'),
         path('recipe/add/', RecipeAddView.as_view(), name='recipe_add'),
         path('recipe/<int:pk>/add_image', RecipeAddImageView.as_view(), name='recipe_add_image'),
+        path('recipes/add_ingredient', RecipeAddIngredientView.as_view(), name='recipe_add_ingredient'),
         ]
 
 appname = "ledger"
